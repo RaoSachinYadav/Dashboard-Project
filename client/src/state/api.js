782f93468c5a0ce81ec747af54b321b1dfb3bf17
Sachin Yadav <sach782001@gmail.com>
2024-06-06 15:12:46 +0530

    code done

---
+++ client/src/state/api.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: [
+    "User",
+    "Sales",
+    "Admins",
+    "Performance",
+    "Dashboard",
+  ],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+    getAdmins: build.query({
+      query: () => "management/admins",
+      providesTags: ["Admins"],
+    }),
+    getUserPerformance: build.query({
+      query: (id) => `management/performance/${id}`,
+      providesTags: ["Performance"],
+    }),
+    getDashboard: build.query({
+      query: () => "general/dashboard",
+      providesTags: ["Dashboard"],
+    }),
+  }),
+});
+
+export const {
+  useGetUserQuery,
+  useGetSalesQuery,
+  useGetAdminsQuery,
+  useGetUserPerformanceQuery,
+  useGetDashboardQuery,
+} = api;
